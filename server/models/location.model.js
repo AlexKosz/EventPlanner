4ecc9f42f34d80e69160a0c52d2adc280d765012
Zanzong43 <alexkoszuta43@gmail.com>
2023-01-09 01:07:41 -0500

    Location Model

--- server/models/location.model.js
+++ server/models/location.model.js
@@ -1,32 +1,80 @@
 const mongoose = require("mongoose");
 
 const LocationSchema = new mongoose.Schema({
-    fullName: {
+    name: {
         type: String,
         required: [true, "Full name is required"]
     },
-    email: {
+    phoneNumber: {
+        type: Number,
+        required: [true, "Phone number is required"]
+    },
+
+    street1: {
+        type: String,
+        required: [true, "street1 is required"],
+    },
+    street2: {
+        type: String,
+        required: [true, "street2 is required"],
+    },
+    city: {
+        type: String,
+        required: [true, "city is required"],
+    },
+    state: {
+        type: String,
+        required: [true, "state is required"],
+    },
+    //string to allow dashes
+    zip: {
+        type: String,
+        required: [true, "state is required"],
+        // TODO
+        // validate: {
+        //     validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+        //     message: "Please enter a valid email"
+        // }
+    },
+    // Only support United States for now
+    // country: {
+    //     type: String,
+    //     required: [true, "state is required"],
+    // },
+
+    parkingInfo: {
         type: String,
         required: false,
-        validate: {
-            //chars + an @ + chars
-            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
-            message: "Please enter a valid email"
-        }
     },
-    phoneNumber: {
+    accessible: {
+        type: Boolean,
+        required: false,
+    },
+    open: {
         type: Number,
         required: false,
     },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-        minlength: [8, "Password must be 8 characters or longer"]
+    close: {
+        type: Number,
+        required: false,
     },
-    // role: {
-    //     type: String,
-    //     required: [true, "Role is required"]
-    // },
+    timeZone: {
+        type: String,
+        required: false,
+    },
+    alcohol: {
+        type: Boolean,
+        required: false,
+    },
+    smoking: {
+        type: Boolean,
+        required: false,
+    },
+    maximumOccupancy: {
+        type: Number,
+        required: false,
+    },
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Location", LocationSchema);
